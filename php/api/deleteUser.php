22249bc319923651de3efb92e4a42ee34db34323
Monsef Eledrisse <zaone035@gmail.com>
2025-06-26 01:36:05 +0200

    Use Eloquent user model in API

--- php/api/deleteUser.php
+++ php/api/deleteUser.php
@@ -1,26 +1,17 @@
 <?php
-require_once("{$_SERVER['DOCUMENT_ROOT']}/php/Class/User.php");
+require_once("{$_SERVER['DOCUMENT_ROOT']}/php/bootstrap.php");
 include "{$_SERVER['DOCUMENT_ROOT']}/php/api/conn.php";
+use App\Models\UserModel;
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
     if(!empty($_POST))
     {
-        $user = new User();
-
-        $count =  $user->getCount($conn);
-
-        foreach ($count as $cn){
-            $number = $cn['nm'];
-        }
-
+        $number = UserModel::count();
         if ($number > 1){
-
-            $result = $user->deletUser($conn,$_POST["user"]);
+            $result = UserModel::where('id', $_POST["user"])->delete();
             echo $result;
-
         }else{
-
             echo "nm0";
         }
 
